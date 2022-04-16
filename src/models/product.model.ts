b3289e173f3e49fe0fade610f0ed1aec250e308a
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-04-16 16:27:09 +0300

    add product schema and controller

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
-import {customAlphabet} from "nanoid";
-import {UserDocument} from "./user.model";
+import { customAlphabet } from "nanoid";
+import { UserDocument } from "./user.model";
 
 const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz0123456789", 10);
 
-export interface ProductDocument extends mongoose.Document {
+export interface ProductInput {
     user: UserDocument["_id"];
     title: string;
     description: string;
     price: number;
     image: string;
-    createAt: Date;
+}
+
+export interface ProductDocument extends ProductInput, mongoose.Document {
+    createdAt: Date;
     updatedAt: Date;
 }
 
@@ -22,11 +25,11 @@ const productSchema = new mongoose.Schema(
             unique: true,
             default: () => `product_${nanoid()}`,
         },
-        user: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
-        title: {type: String, required: true},
-        description: {type: String, required: true},
-        price: {type: Number, required: true},
-        image: {type: String, required: true},
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        title: { type: String, required: true },
+        description: { type: String, required: true },
+        price: { type: Number, required: true },
+        image: { type: String, required: true },
     },
     {
         timestamps: true,
