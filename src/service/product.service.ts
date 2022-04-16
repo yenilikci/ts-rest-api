412d7de36af050abc67a624856c15535c9606eab
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-04-16 08:00:45 +0300

    add findProduct service func

--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -1,11 +1,13 @@
-import {
-    DocumentDefinition,
-    FilterQuery,
-    QueryOptions,
-    UpdateQuery,
-} from "mongoose";
+import {DocumentDefinition, FilterQuery, QueryOptions,} from "mongoose";
 import ProductModel, {ProductDocument} from "../models/product.model";
 
 export async function createProduct(input: DocumentDefinition<Omit<ProductDocument, "createdAt" | "updatedAt">>) {
     return ProductModel.create(input);
+}
+
+export async function findProduct(
+    query: FilterQuery<ProductDocument>,
+    options: QueryOptions = {lean: true}
+) {
+    return ProductModel.findOne(query, {}, options);
 }
