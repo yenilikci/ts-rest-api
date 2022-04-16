ed5b77d6045e5bf4fc54cb83f0c29fdad5daf133
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-04-16 05:35:56 +0300

    add session controller, schema, service and jwt utils

---
+++ src/controller/session.controller.ts
@@ -0,0 +1,27 @@
+import {Request, Response} from "express";
+import {validatePassword} from "../service/user.service";
+import {createSession} from "../service/session.service";
+import {signJwt} from "../utils/jwt.utils";
+import config from "config";
+
+export async function createUserSessionHandler(req: Request, res: Response) {
+    // validate the user's password
+    const user = await validatePassword(req.body)
+    if (!user) {
+        return res.status(401).send("Invalid email or password");
+    }
+    // create a session
+    const session = await createSession(user._id, req.get("user-agent") || "")
+    // create an access token
+    const accessToken = signJwt(
+        {...user, session: session._id},
+        {expiresIn: config.get('accessTokenTtl')} // 15 minutes
+    )
+    // create a refresh token
+    const refreshToken = signJwt(
+        {...user, session: session._id},
+        {expiresIn: config.get('refreshTokenTtl')} // 15 minutes
+    )
+    // return access & refresh tokens
+    return res.send({accessToken, refreshToken})
+}
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -42,5 +42,5 @@ userSchema.methods.comparePassword = async function (
     return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
 };
 
-const UserModel = mongoose.model('User', userSchema);
+const UserModel = mongoose.model<UserDocument>('User', userSchema);
 export default UserModel;
